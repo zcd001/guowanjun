dd4c3d3653ea8fbf3b0cbe12345e61467d6096d7
zcd001 <1121088348@qq.com>
2025-04-01 21:52:42 +0800

    up

---
+++ static/js/baidutongji0522.js
@@ -0,0 +1,7 @@
+﻿var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?fb54f6c29c0441d33943f55a99f74e3a";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
